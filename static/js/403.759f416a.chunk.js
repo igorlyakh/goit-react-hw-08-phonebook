0a4bdc438f3a512ab66cbeb757618c118ebcaada
igorlyakh <igorlyah060@gmail.com>
2023-12-22 21:49:27 +0000

    Deploying to gh-pages from @ igorlyakh/goit-react-hw-08-phonebook@c29a954b76dc688723bfb5fe584af5efee0d232e 🚀

---
+++ static/js/403.759f416a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkphonebook=self.webpackChunkphonebook||[]).push([[403],{403:function(n,e,o){o.r(e),o.d(e,{default:function(){return c}});var t,p=o(168),r=o(547).ZP.h1(t||(t=(0,p.Z)(["\n  margin-top: 50px;\n  text-align: center;\n"]))),u=o(184),c=function(){return(0,u.jsx)(r,{children:"Welcome to home page of phonebook app! \ud83c\udfe0"})}}}]);
+//# sourceMappingURL=403.759f416a.chunk.js.map
